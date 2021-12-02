020c2f098d3bcbf87427a3ba34af5b1d94d09f2a
Steve Taggart <11730266+stevetaggart@users.noreply.github.com>
2021-12-02 14:31:20 -0700

    Day 2 solutions

--- AdventOfCode/Solutions/Year2021/Day02/Day02.cs
+++ AdventOfCode/Solutions/Year2021/Day02/Day02.cs
@@ -1,26 +1,60 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace AdventOfCode.Solutions.Year2021
 {
 
     class Day02 : ASolution
     {
+        readonly string[] instructions;
 
         public Day02() : base(02, 2021, "")
         {
-
+             instructions = Input.SplitByNewline();
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var horizontalPosition = 0;
+            var depth = 0;
+            foreach (var instruction in instructions)
+            {
+                var split = instruction.Split();
+                var direction = split[0];
+                var amount = Convert.ToInt32(split[1]);
+                _ = direction switch
+                {
+                    "forward" => horizontalPosition += amount,
+                    "down" => depth += amount,
+                    "up" => depth -= amount,
+                    _ => throw new Exception("Invalid instruction"),
+                };
+                Console.WriteLine($"Direction: {direction} Amount: {amount}");
+            }
+            return $"{horizontalPosition * depth}";
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var horizontalPosition = 0;
+            var depth = 0;
+            var aim = 0;
+            foreach (var instruction in instructions)
+            {
+                var split = instruction.Split();
+                var direction = split[0];
+                var amount = Convert.ToInt32(split[1]);
+                if (direction == "forward")
+                {
+                    horizontalPosition += amount;
+                    depth += aim * amount;
+                }
+                if (direction == "down")
+                    aim += amount;
+                if (direction == "up")
+                    aim -= amount;
+                Console.WriteLine($"Direction: {direction} Amount: {amount} Aim: {aim}");
+            }
+            return $"{horizontalPosition * depth}";
         }
     }
 }
