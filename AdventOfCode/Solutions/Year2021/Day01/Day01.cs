c16729dce1722b21b24bfe9bd9251ad2b50e3088
Steve Taggart <11730266+stevetaggart@users.noreply.github.com>
2021-12-02 14:16:34 -0700

    Day 1 solutions

--- AdventOfCode/Solutions/Year2021/Day01/Day01.cs
+++ AdventOfCode/Solutions/Year2021/Day01/Day01.cs
@@ -1,26 +1,55 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace AdventOfCode.Solutions.Year2021
 {
 
     class Day01 : ASolution
     {
+        readonly string[] measurements;
 
         public Day01() : base(01, 2021, "")
         {
-
+            measurements = Input.SplitByNewline();
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var increaseCount = 0;
+            var lastMeasurement = -1;
+            foreach (var measurement in measurements)
+            {
+                var currentMeasurement = Convert.ToInt16(measurement);
+                if (lastMeasurement == -1)
+                {
+                    Console.WriteLine($"First measurement is: {currentMeasurement}");
+                }
+                else if (currentMeasurement > lastMeasurement)
+                {
+                    increaseCount++;
+                }
+                lastMeasurement = currentMeasurement;
+            }
+            return increaseCount.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var increaseCount = 0;
+            var lastSum = -1;
+            for (var i=0;i < measurements.Length - 2;i++)
+            {
+                var currentSum = Convert.ToInt16(measurements[i]) + Convert.ToInt16(measurements[i + 1]) + Convert.ToInt16(measurements[i + 2]);
+                if (lastSum == -1)
+                {
+                    Console.WriteLine($"First sum is: {currentSum}");
+                }
+                else if (currentSum > lastSum)
+                {
+                    increaseCount++;
+                }
+                lastSum = currentSum;
+            }
+            return increaseCount.ToString();
         }
     }
 }
